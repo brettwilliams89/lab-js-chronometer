e302c72825dc4c40f5e6904c4ce0e287ea4d26dc
Brett Williams <brett.williams.89@gmail.com>
2023-04-04 22:01:51 -0400

    added to src chrono.js

--- src/chronometer.js
+++ src/chronometer.js
@@ -1,33 +1,52 @@
 class Chronometer {
   constructor() {
-    // ... your code goes here
+    this.currentTime = 0; // sets current time to 0
+    this.intervalId = null; // sets intervalId to null
   }
 
   start(printTimeCallback) {
-    // ... your code goes here
+    this.intervalId =  setInterval( () => { // creates timer that runs a callback function
+      this.currentTime++; // is incrementing the currentTime property of the Chronometer.
+      if(printTimeCallback){ // sets condition if printTimeCallBack is called, return the value
+        printTimeCallback()
+      }   
+    }, 1000 ) // set interval to 1 sec
   }
 
   getMinutes() {
-    // ... your code goes here
+    let count = this.currentTime / 60 // takes the current time and divides by 60 to get minutes
+    let minuteCount = Math.floor(count); // rounds to nearest integer
+    return minuteCount;
   }
 
   getSeconds() {
-    // ... your code goes here
+    if (this.currentTime === 0) { // sets condition if current time === 0, return 0.
+      return 0;
+    } else {
+      return this.currentTime % 60;//sets condition if current time is other than 0, % 60 to get seconds value.
+    }
   }
 
   computeTwoDigitNumber(value) {
-    // ... your code goes here
+    if (value < 10) { // sets condition if value is less than 10, return "0" and add the vlaue behind as a string.
+      return "0" + value;
+    } else {
+      return value.toString();//returns value and converts to a string for anything  = or greater than 10
+    }
   }
+  
 
   stop() {
-    // ... your code goes here
+    return clearInterval(this.intervalId); //uses clearInterval to cancel timer set by setInterval
   }
 
   reset() {
-    // ... your code goes here
+    return this.currentTime= 0; //resets time to this current time
   }
 
   split() {
-    // ... your code goes here
+    let minutes = this.computeTwoDigitNumber(this.getMinutes());
+    let seconds = this.computeTwoDigitNumber(this.getSeconds());
+    return `${minutes}:${seconds}`
   }
 }
